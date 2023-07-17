fbc882f0042bf0d23f485e5634ed619dd54a7af1
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-18 00:29:59 +0200

    añadido hook useSearch

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
+import { useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
-import responseMovies from "./mocks/results.json";
+import { searchMovies } from "./services/movies";
 
 function App() {
-  const movies = responseMovies.Search;
 
-  const mappedMovies = movies.map((movie) => ({
-    id: movie.imdbID,
-    title: movie.Title,
-    year: movie.Year,
-    poster: movie.Poster,
-  }));
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    searchMovies(event.target.value);
+  };
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value;
+    setSearch(newSearch);
+  };
 
   return (
     <div className="page">
       <header>
         <h1>Buscador de películas</h1>
-        <form>
-          <input placeholder="Avengers, matrix..." />
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange}
+            name="query"
+            value={search}
+            placeholder="Avengers, matrix..."
+          />
           <button>Buscar</button>
         </form>
       </header>
 
       <main>
-        <Movies movies={mappedMovies} />
+        <Movies movies={movies} />
       </main>
     </div>
   );
