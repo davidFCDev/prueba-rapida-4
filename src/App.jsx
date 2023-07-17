0f7d52bf2b3680c793de9169318c9254a26a81c7
David Fernández <davidfernandezcomesana@gmail.com>
2023-07-18 00:09:00 +0200

    mapeado de mock

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import "./App.css";
+import responseMovies from "./mocks/results.json";
 
 function App() {
+  const movies = responseMovies.Search;
+  const hasMovies = movies?.length > 0;
+
   return (
     <div className="page">
       <header>
@@ -12,7 +16,19 @@ function App() {
       </header>
 
       <main>
-        
+        {hasMovies ? (
+          <ul className="movies">
+            {movies.map((movie) => (
+              <li key={movie.imdbID} className="movie">
+                <h3>{movie.Title}</h3>
+                <p>{movie.Year}</p>
+                <img src={movie.Poster} alt={movie.Title} />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No se han encontrado resultados</p>
+        )}
       </main>
     </div>
   );
